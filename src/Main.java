50384808ac25472c34b41ed81afd51b91e5de8dc
suleymanunal <125310774+suleyman4332@users.noreply.github.com>
2023-03-28 16:42:50 +0300

    ekleme2803

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Hello GitUp");
+        System.out.println("yeni eklemeler ");
+        int sayi=35;
+        int sayi2=45;
+        int toplam=sayi+sayi2;
+        System.out.println("toplam = " + toplam);
 
     }
 }
--- src/helloGitUp/helloGitUp01.java
+++
@@ -1,8 +0,0 @@
-package helloGitUp;
-
-public class helloGitUp01 {
-    public static void main(String[] args) {
-        System.out.println("yeni deneme");
-
-    }
-}
